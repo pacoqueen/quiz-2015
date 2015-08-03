95e47829035782d52760b23fe53c411430d504cb
Francisco José Rodríguez Bogado <bogado@qinn.es>
2015-08-03 09:18:15 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,14 @@
 var models = require('../models/models.js');
 
 // GET /quizes/question
-exports.question = function(reg, res) {
+/* exports.question = function(reg, res) {
     models.Quiz.findAll().then(function(quiz){
       res.render('quizes/question', {pregunta: quiz[0].pregunta});
     })
-};
+}; */
 
 // GET /quizes/answer
-exports.answer = function(req, res) {
+/* exports.answer = function(req, res) {
     models.Quiz.findAll().then(function(quiz) {
         if (req.query.respuesta === quiz[0].respuesta){
             res.render('quizes/answer', {respuesta: 'Correcto'});
@@ -16,5 +16,31 @@ exports.answer = function(req, res) {
             res.render('quizes/answer', {respuesta: 'Incorrecto'});
         }
     })
+};*/
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz) {
+        res.render('quizes/show', {quiz: quiz});
+    })
 };
 
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+    models.Quiz.findById(req.params.quizId).then(function(quiz) {
+        if (req.query.respuesta === quiz.respuesta) {
+            res.render('quizes/answer',
+                       {quiz: quiz, respuesta: 'Correcto'});
+        } else {
+            res.render('quizes/answer',
+                       {quiz: quiz, respuesta: 'Incorrecto'});
+        }
+    })
+};
+
+// GET /quizes
+exports.index = function(req, res){
+    models.Quiz.findAll().then(function(quizes){
+        res.render('quizes/index.ejs', {quizes: quizes});
+    })
+};
--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,19 @@ router.get('/', function(req, res, next) {
 });
 
 // GET /quizes/question
-router.get('/quizes/question', quizController.question);
+/* router.get('/quizes/question', quizController.question); */
 
 // GET /quizes/answer
-router.get('/quizes/answer', quizController.answer);
+/* router.get('/quizes/answer', quizController.answer); */
 
 // GET /author
 router.get('/author', function(req, res) {
     res.render('author');
 });
+
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
