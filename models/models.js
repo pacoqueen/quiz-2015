e5efe80312ada4438a7a185cccb674bcd570c56e
Paco Queen <pacoqueen@gmail.com>
2015-08-03 02:09:10 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,45 @@
+var path=require('path');
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar base de datos SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+                              {dialect: protocol,
+                               protocol: protocol,
+                               port: port,
+                               host: host,
+                               storage: storage, // solo SQLite (.env)
+                               omitNull: true    // solo Postgres
+    }
+    );
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+exports.Quiz = Quiz;  // Exportar definición de la tabla Quiz
+
+// sequelize.sync()     // crea e inicializa tabla de preguntas en BD
+sequelize.sync().then(function() {
+  // success(...) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function(count){
+    if (count === 0){  // la tabla se inicializa solo si está vacía
+      Quiz.create({pregunta: 'Capital de Italia',
+                   respuesta: 'Roma'
+                  })
+       .then(function(){console.log('Base de datos inicializada.')});
+      };
+    });
+  });
---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+// Definicion del modelo de Quiz
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+    'Quiz',
+    { pregunta: DataTypes.STRING,
+      respuesta: DataTypes.STRING,
+    });
+}
