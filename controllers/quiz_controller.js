704b01ab136defebddb442b87ef9cb0345a94810
Francisco José Rodríguez Bogado <bogado@qinn.es>
2015-08-03 09:28:32 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,14 +20,14 @@ var models = require('../models/models.js');
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-    models.Quiz.findById(req.params.quizId).then(function(quiz) {
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
         res.render('quizes/show', {quiz: quiz});
     })
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-    models.Quiz.findById(req.params.quizId).then(function(quiz) {
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
         if (req.query.respuesta === quiz.respuesta) {
             res.render('quizes/answer',
                        {quiz: quiz, respuesta: 'Correcto'});
