ed1b942bc4a4ba8e6ad57e3644091b8dc13bfcf4
Francisco José Rodríguez Bogado <bogado@qinn.es>
2015-08-03 09:18:15 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -38,6 +38,9 @@ sequelize.sync().then(function() {
     if (count === 0){  // la tabla se inicializa solo si está vacía
       Quiz.create({pregunta: 'Capital de Italia',
                    respuesta: 'Roma'
+                  });
+      Quiz.create({pregunta: 'Capital de Portugal',
+                   respuesta: 'Lisboa'
                   })
        .then(function(){console.log('Base de datos inicializada.')});
       };
