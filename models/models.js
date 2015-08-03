834205b8f5937db4d11643fb085ab2dfe9eafe25
Francisco José Rodríguez Bogado <bogado@qinn.es>
2015-08-03 12:59:23 +0200

    P2P Módulo 8: temas en preguntas.

--- models/models.js
+++ models/models.js
@@ -37,10 +37,12 @@ sequelize.sync().then(function() {
   Quiz.count().then(function(count){
     if (count === 0){  // la tabla se inicializa solo si está vacía
       Quiz.create({pregunta: 'Capital de Italia',
-                   respuesta: 'Roma'
+                   respuesta: 'Roma',
+                   tema: 'humanidades'
                   });
       Quiz.create({pregunta: 'Capital de Portugal',
-                   respuesta: 'Lisboa'
+                   respuesta: 'Lisboa',
+                   tema: 'humanidades'
                   })
        .then(function(){console.log('Base de datos inicializada.')});
       };
