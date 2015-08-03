cd5cbf0c56af68b412496f4b36ebb9ea9d49b36d
Francisco José Rodríguez Bogado <bogado@qinn.es>
2015-08-03 12:22:00 +0200

    Borrado de preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -95,3 +95,10 @@ exports.update = function(req, res){
         }
     );
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res){
+    req.quiz.destroy().then(function(){
+        res.redirect('/quizes');
+    }).catch(function(error){next(error)});
+};
