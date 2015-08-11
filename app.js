49d20a1c25cef886cf2a50766f71a68190329799
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2015-08-11 09:21:11 +0200

    Autenticación

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -23,9 +24,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 /* app.use(bodyParser.urlencoded({ extended: false })); */
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helpers dinámicos
+app.use(function(req, res, next){
+    // guardar path en session.redir para después de login
+    if (!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;
+    }
+    // Hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', routes);
 
