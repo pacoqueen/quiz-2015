166b8c1766b18e021750cce596d4f93e07e8e0d8
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2015-08-11 09:21:11 +0200

    Autenticación

---
+++ controllers/session_controller.js
@@ -0,0 +1,45 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+// Get /login   -- Formulario de login
+exports.new = function(req, res) {
+    var errors = req.session.errors || {};
+    req.session.errors = {};
+
+    res.render('sessions/new', {errors: errors});
+};
+
+// POST /login   -- Crear la sesion si usuario se autentica
+exports.create = function(req, res) {
+
+    var login = req.body.login;
+    var password = req.body.password;
+
+    var userController = require('./user_controller');
+    userController.autenticar(login, password, function(error, user) {
+
+        if (error) {  // si hay error retornamos mensajes de error de sesión
+            req.session.errors = [{"message": 'Se ha producido un error: '+error}];
+            res.redirect("/login");
+            return;
+        }
+
+        // Crear req.session.user y guardar campos   id  y  username
+        // La sesión se define por la existencia de:    req.session.user
+        req.session.user = {id:user.id, username:user.username, isAdmin:user.isAdmin};
+
+        res.redirect(req.session.redir.toString());// redirección a path anterior a login
+    });
+};
+
+// DELETE /logout   -- Destruir sesion
+exports.destroy = function(req, res) {
+    delete req.session.user;
+    res.redirect(req.session.redir.toString()); // redirect a path anterior a login
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de sesión
+router.get('/login', sessionController.new);        // formulario de login
+router.post('/login', sessionController.create);    // crear sesión
+router.get('/logout', sessionController.destroy);   // destruir sesión
 
 // GET /author
 router.get('/author', function(req, res) {
@@ -21,11 +27,11 @@ router.get('/author', function(req, res) {
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
