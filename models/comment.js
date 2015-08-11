1f0563301fa860456ae32e803f32013c86602016
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2015-08-11 10:28:52 +0200

    Moderación de comentarios

--- models/comment.js
+++ models/comment.js
@@ -5,9 +5,13 @@ module.exports = function(sequelize, DataTypes){
     return sequelize.define(
             'Comment',
             {texto: {
-                        type: DataTypes.STRING,
-                        validate: {notEmpty: {msg: "-> Falta comentario"}}
-                    }
+                 type: DataTypes.STRING,
+                 validate: {notEmpty: {msg: "-> Falta comentario"}}
+             },
+             publicado: {
+                 type: DataTypes.BOOLEAN,
+                 defaultValue: false
+             }
             }
-            );
+        );
 };
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load);  // autoload: commentID
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);        // formulario de login
@@ -35,5 +36,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+        sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
